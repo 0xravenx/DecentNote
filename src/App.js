6bc906726c518870c6ff7f0fd402aebd65d82eff
0xravenx <zvyvgmlxyg316@hotmail.com>
2025-04-03 19:27:43 +0800

    initial project setup with react basics
    - added package.json with react and web3 deps
    - setup basic react app structure
    - added dark theme styling for crypto vibes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DecentNote</h1>
+        <p>Decentralized Note Taking</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
